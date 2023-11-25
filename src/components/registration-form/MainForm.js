02d823c1c08248e17ca79ad30555afb4e2bc1a16
dipakpadule <dipakpadule7272@gmail.com>
2023-11-25 10:15:17 +0530

    validation done data received

--- src/components/registration-form/MainForm.js
+++ src/components/registration-form/MainForm.js
@@ -14,135 +14,210 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 
+import * as yup from "yup";
+import { Formik } from "formik";
+const userSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required("name is required")
+    .min(3, "name must be at least 3 chars"),
+  email: yup.string().email("enter valid email").required("email is required"),
+  password: yup
+    .string()
+    .min(8, "password must be at least 8 chats")
+    .required("password is required"),
+  mobile: yup
+    .string()
+    .matches(/^[0-9]{10}$/, "mobile must be 10 digits")
+    .required("mobile is required"),
+});
+
 const MainForm = () => {
   return (
     <Box sx={{ padding: 4 }}>
       <Typography variant="h5" sx={{ mb: 3, mt: 2 }}>
         Find a job & grow your career{" "}
       </Typography>
-      <form>
-        <Grid
-          container
-          spacing={2}
-          sx={{ display: "flex", flexDirection: "column" }}
-        >
-          <Grid item>
-            <InputLabel htmlFor="fullname" sx={{ fontWeight: "bold" }}>
-              Full name
-            </InputLabel>
-            <TextField
-              variant="outlined"
-              fullWidth
-              label="What is your name?"
-              type="text"
-              name="name"
-              sx={{ mt: 1 }}
-            ></TextField>
-          </Grid>
-          <Grid item>
-            <InputLabel htmlFor="email" sx={{ fontWeight: "bold" }}>
-              Email ID
-            </InputLabel>
-            <TextField
-              variant="outlined"
-              fullWidth
-              label="Tell us your Email ID"
-              type="email"
-              name="email"
-              sx={{ mt: 1 }}
-            ></TextField>
-          </Grid>
-          <Grid item>
-            <InputLabel htmlFor="password" sx={{ fontWeight: "bold" }}>
-              Password
-            </InputLabel>
-            <TextField
-              variant="outlined"
-              fullWidth
-              label="Create a password for your account"
-              type="password"
-              name="password"
-              sx={{ mt: 1 }}
-            ></TextField>
-          </Grid>
-          <Grid item>
-            <InputLabel htmlFor="mobilenumber" sx={{ fontWeight: "bold" }}>
-              Mobile number
-            </InputLabel>
-            <TextField
-              variant="outlined"
-              fullWidth
-              label="Mobile Number"
-              type="tel"
-              name="mobile"
-              sx={{ mt: 1 }}
-            ></TextField>
-          </Grid>
-          <Grid item>
-            <InputLabel htmlFor="workstatus" sx={{ fontWeight: "bold", mb: 1 }}>
-              Work Status
-            </InputLabel>
-            <Box sx={{ display: "flex", gap: 2 }}>
-              <Card
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+          mobile: "",
+          whatsapp: false,
+        }}
+        validationSchema={userSchema}
+        onSubmit={(values) => {
+          console.log("user: ", values);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+          isValid,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Grid
+              container
+              spacing={2}
+              sx={{ display: "flex", flexDirection: "column" }}
+            >
+              <Grid item xs={12}>
+                <Typography sx={{ fontWeight: "bold" }}>Full name</Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  autoFocus
+                  autoComplete="off"
+                  label="What is your name?"
+                  value={values.name}
+                  type="text"
+                  name="name"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched?.name && errors?.name ? true : false}
+                  helperText={touched?.name && errors?.name}
+                  sx={{ mt: 1 }}
+                ></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography sx={{ fontWeight: "bold" }}>Email ID</Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  autoComplete="off"
+                  label="Tell us your Email ID"
+                  value={values.email}
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched?.email && errors?.email ? true : false}
+                  helperText={touched?.email && errors?.email}
+                  sx={{ mt: 1 }}
+                ></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography sx={{ fontWeight: "bold" }}>Password</Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  label="Create a password for your account"
+                  value={values.password}
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched?.password && errors?.password ? true : false}
+                  helperText={touched?.password && errors?.password}
+                  sx={{ mt: 1 }}
+                ></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography htmlFor="mobile" sx={{ fontWeight: "bold" }}>
+                  Mobile number
+                </Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  label="Mobile Number"
+                  value={values.mobile}
+                  type="number"
+                  name="mobile"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched?.mobile && errors?.mobile ? true : false}
+                  helperText={touched?.mobile && errors?.mobile}
+                  sx={{ mt: 1 }}
+                ></TextField>
+              </Grid>
+              {/* Work Status */}
+              <Grid item>
+                <Typography
+                  htmlFor="workstatus"
+                  sx={{ fontWeight: "bold", mb: 1 }}
+                >
+                  Work Status
+                </Typography>
+                <Box sx={{ display: "flex", gap: 2 }}>
+                  <Card
+                    sx={{
+                      minWidth: 320,
+                      border: "1px solid black",
+                      borderRadius: 5,
+                    }}
+                  >
+                    <CardContent>
+                      <Typography variant="h6" sx={{ fontSize: 15 }}>
+                        I'm Experienced
+                      </Typography>
+                      <Typography variant="body2">
+                        I have work experience (excluding internships)
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                  <Card
+                    sx={{
+                      minWidth: 320,
+                      border: "1px solid #fff",
+                      borderRadius: 5,
+                    }}
+                  >
+                    <CardContent>
+                      <Typography variant="h6" sx={{ fontSize: 15 }}>
+                        I'm a Fresher
+                      </Typography>
+                      <Typography variant="body2">
+                        I am a student/ Haven't worked after graduation
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid item sx={{ display: "flex", alignItems: "center" }}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    name="whatsapp"
+                    onChange={() => (values.whatsapp = !values.whatsapp)}
+                  />
+                }
+                label="Send me important updates on"
+              />
+              <Link
+                underline="none"
+                color="black"
                 sx={{
-                  minWidth: 320,
-                  border: "1px solid black",
-                  borderRadius: 5,
+                  display: "flex",
+                  alignItems: "center",
+                  cursor: "pointer",
                 }}
               >
-                <CardContent>
-                  <Typography variant="h6" sx={{ fontSize: 15 }}>
-                    I'm Experienced
-                  </Typography>
-                  <Typography variant="body2">
-                    I have work experience (excluding internships)
-                  </Typography>
-                </CardContent>
-              </Card>
-              <Card
-                sx={{
-                  minWidth: 320,
-                  border: "1px solid #fff",
-                  borderRadius: 5,
-                }}
-              >
-                <CardContent>
-                  <Typography variant="h6" sx={{ fontSize: 15 }}>
-                    I'm a Fresher
-                  </Typography>
-                  <Typography variant="body2">
-                    I am a student/ Haven't worked after graduation
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Box>
-          </Grid>
-        </Grid>
-        <Grid item sx={{ display: "flex", alignItems: "center" }}>
-          <FormControlLabel
-            control={<Checkbox name="whatsapp" />}
-            label="Send me important updates on"
-          />
-          <Link
-            underline="none"
-            color="black"
-            sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}
-          >
-            <WhatsAppIcon variant="div" sx={{ color: "green", fontSize: 20 }} />
-            <Typography> WhatsApp</Typography>
-          </Link>
-        </Grid>
+                <WhatsAppIcon
+                  variant="div"
+                  sx={{ color: "green", fontSize: 20 }}
+                />
+                <Typography> WhatsApp</Typography>
+              </Link>
+            </Grid>
 
-        <Grid item>
-          <Typography sx={{ mt: 2 }}>
-            By clicking Register, you agree to the Terms and Conditions &
-            Privacy of ToppersSkill.com
-          </Typography>
-          <Button variant="contained" sx={{ mt: 3 }}>
-            Register Now
-          </Button>
-        </Grid>
-      </form>
+            <Grid item>
+              <Typography sx={{ mt: 2 }}>
+                By clicking Register, you agree to the Terms and Conditions &
+                Privacy of ToppersSkill.com
+              </Typography>
+              <Button variant="contained" type="submit" sx={{ mt: 3 }}>
+                Register Now
+              </Button>
+            </Grid>
+          </form>
+        )}
+      </Formik>
     </Box>
   );
 };
